69dc9f72db145247b59114322e3106b18eb3ec30
Abdullah Al Noman <abdullahalnomancse@gmail.com>
2024-04-22 09:36:58 +0600

    mainly done

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,17 @@ export default function App() {
 function ContextValue() {
   const { count, setCount } = useCount();
   return (
-    <div className="flex justify-center items-center flex-col w-[300px]">
+    <div className="flex justify-center items-center flex-col w-screen h-screen bg-black text-slate-400">
       <h1 className="text-3xl font-bold underline underline-offset-8 ">
         React & TypeScript
       </h1>
-      <h2 className="p-2 text-xl">{count}</h2>
+      <h2 className="p-2 text-xl">
+        <span>Context -- </span>
+        {count}
+      </h2>
       <Button onClick={() => setCount((prevous) => prevous + 1)}>
         Increase
       </Button>
     </div>
   );
-}
+} 
